ea72f84eb57e9f0fb55fadf06a5b4bbaa083fb98
Farah Bali <balifarah2001@gmail.com>
2024-12-28 22:46:07 +0100

    init : firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,19 +4,18 @@ import { getFirestore } from "firebase/firestore";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAOWHBpPhKoNhcGFKHH_Q_0AtL2gV-imgQ',
-  authDomain: 'production-a9404.firebaseapp.com',
-  databaseURL: 'https://production-a9404.firebaseio.com',
-  projectId: 'production-a9404',
-  storageBucket: 'production-a9404.appspot.com',
-  messagingSenderId: '525472070731',
-  appId: '1:525472070731:web:ee873bd62c0deb7eba61ce',
+  apiKey: "AIzaSyDI0SAlMvlNfSop56ClEEfBvNOcZTzLGHE",
+  authDomain: "heating-pad.firebaseapp.com",
+  projectId: "heating-pad",
+  storageBucket: "heating-pad.firebasestorage.app",
+  messagingSenderId: "883615324170",
+  appId: "1:883615324170:web:251989d0ad6a6aa541a7f3",
+  measurementId: "G-95T63M6XWE"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Auth with AsyncStorage persistence
+
 const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(AsyncStorage)
 });
